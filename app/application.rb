be653d01397116706282f9dab13aa56e75b1833d
Matthew Bartl <mattbartl@gmail.com>
2019-04-22 01:08:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,10 @@ class Application
   def call(env)
     clock = Time.now.to_s.split(' ')[1]
     if clock.split(':')[0].to_i > 12
-      resp.write()
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
   end
   
 end
